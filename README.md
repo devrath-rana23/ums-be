645491a2946014fed7411fc46be7da5b16812c98
Devrath Rana <chintusinghrana1234567890@gmail.com>
2023-02-12 20:33:54 +0530

    make controller files

--- README.md
+++ README.md
@@ -23,3 +23,18 @@
 # sudo chmod +x ./seeder.sh
 
 # ./seeder.sh
+
+# make resource controller for Employee, skill, and role
+
+php artisan make:controller Api/RoleController --resource
+php artisan make:controller Api/SkillController --resource
+php artisan make:controller Api/EmployeeController --resource
+
+# make controller for authentication, authorization flow
+
+php artisan make:controller Api/AuthController
+
+# configure api.php file
+
+# integrate socialite
+
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource("roles", App\Http\Controllers\Api\RoleController::class);
+Route::apiResource("skills", App\Http\Controllers\Api\SkillController::class);
+Route::get("/employee", [App\Http\Controllers\Api\EmployeeController::class, 'index'])->name('employee.list');
+Route::get("/employee/{$id}", [App\Http\Controllers\Api\EmployeeController::class, 'show'])->name('employee.details');
+Route::get("/employee/edit/{$id}", [App\Http\Controllers\Api\EmployeeController::class, 'edit'])->name('employee.edit');
+Route::put("/employee/{$id}", [App\Http\Controllers\Api\EmployeeController::class, 'update'])->name('employee.update');
+Route::get("/employee/create/{$id}", [App\Http\Controllers\Api\EmployeeController::class, 'create'])->name('employee.create');
+Route::post("/employee/{$id}", [App\Http\Controllers\Api\EmployeeController::class, 'store'])->name('employee.store');
+Route::delete("/employee/{$id}", [App\Http\Controllers\Api\EmployeeController::class, 'destroy'])->name('employee.destroy');
