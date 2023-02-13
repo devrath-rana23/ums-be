9a3ca66c1695df9ddc34ea6a4ace7500f7cbeeb7
Devrath Rana <chintusinghrana1234567890@gmail.com>
2023-02-14 00:19:21 +0530

    done

--- routes/api.php
+++ routes/api.php
@@ -19,31 +19,38 @@ Route::prefix('v1')->group(static function () {
             Route::get(
                 "/employee",
                 [App\Http\Controllers\Api\EmployeeController::class, 'index']
-            )->name('employee.list');
+            )
+                ->name('employee.list');
             Route::get(
                 "/" . config('constants.api.employee_common_string'),
                 [App\Http\Controllers\Api\EmployeeController::class, 'show']
-            )->name('employee.details');
+            )
+                ->name('employee.details');
             Route::get(
                 "/employee/edit/{id}",
                 [App\Http\Controllers\Api\EmployeeController::class, 'edit']
-            )->name('employee.edit');
+            )
+                ->name('employee.edit');
             Route::put(
                 "/" . config('constants.api.employee_common_string'),
                 [App\Http\Controllers\Api\EmployeeController::class, 'update']
-            )->name('employee.update');
+            )
+                ->name('employee.update');
             Route::get(
                 "/employee/create/{id}",
                 [App\Http\Controllers\Api\EmployeeController::class, 'create']
-            )->name('employee.create');
+            )
+                ->name('employee.create');
             Route::post(
                 "/" . config('constants.api.employee_common_string'),
                 [App\Http\Controllers\Api\EmployeeController::class, 'store']
-            )->name('employee.store');
+            )
+                ->name('employee.store');
             Route::delete(
                 "/" . config('constants.api.employee_common_string'),
                 [App\Http\Controllers\Api\EmployeeController::class, 'destroy']
-            )->name('employee.destroy');
+            )
+                ->name('employee.destroy');
         });
         Route::middleware([CheckSuperAdmin::class])->group(function () {
             Route::apiResource("roles", App\Http\Controllers\Api\RoleController::class);
