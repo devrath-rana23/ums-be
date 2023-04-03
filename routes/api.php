9459225d409a4e09f556215fd16d62f9b4e9dd80
Devrath Rana <devrath.rana@kellton.com>
2023-04-03 12:35:02 +0530

    fix

--- routes/api.php
+++ routes/api.php
@@ -15,37 +15,35 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(static function () {
     Route::group(['middleware' => ['auth:sanctum']], static function () {
-        Route::middleware([CheckAdmin::class])->group(function () {
-            Route::get(
-                "/employee",
-                [App\Http\Controllers\Api\EmployeeController::class, 'index']
-            )
-                ->name('employee.list');
-            Route::get(
-                "/employee/edit/{id}",
-                [App\Http\Controllers\Api\EmployeeController::class, 'edit']
-            )
-                ->name('employee.edit');
-            Route::put(
-                "/" . config('constants.api.employee_common_string'),
-                [App\Http\Controllers\Api\EmployeeController::class, 'update']
-            )
-                ->name('employee.update');
-            Route::post(
-                "/employee",
-                [App\Http\Controllers\Api\EmployeeController::class, 'store']
-            )
-                ->name('employee.store');
-            Route::delete(
-                "/" . config('constants.api.employee_common_string'),
-                [App\Http\Controllers\Api\EmployeeController::class, 'destroy']
-            )
-                ->name('employee.destroy');
-            Route::prefix('export-csv')->group(function () {
-                Route::get('skill', [App\Http\Controllers\Api\ExportCsvController::class, 'exportSkills']);
-            });
+        Route::get(
+            "/employee",
+            [App\Http\Controllers\Api\EmployeeController::class, 'index']
+        )
+            ->name('employee.list');
+        Route::get(
+            "/employee/edit/{id}",
+            [App\Http\Controllers\Api\EmployeeController::class, 'edit']
+        )
+            ->name('employee.edit');
+        Route::put(
+            "/" . config('constants.api.employee_common_string'),
+            [App\Http\Controllers\Api\EmployeeController::class, 'update']
+        )
+            ->name('employee.update');
+        Route::post(
+            "/employee",
+            [App\Http\Controllers\Api\EmployeeController::class, 'store']
+        )
+            ->name('employee.store');
+        Route::delete(
+            "/" . config('constants.api.employee_common_string'),
+            [App\Http\Controllers\Api\EmployeeController::class, 'destroy']
+        )
+            ->name('employee.destroy');
+        Route::prefix('export-csv')->group(function () {
+            Route::get('skill', [App\Http\Controllers\Api\ExportCsvController::class, 'exportSkills']);
         });
-        Route::middleware([CheckSuperAdmin::class])->group(function () {
+        Route::middleware([CheckAdmin::class])->group(function () {
             Route::apiResource("roles", App\Http\Controllers\Api\RoleController::class);
             Route::apiResource("skills", App\Http\Controllers\Api\SkillController::class);
         });
